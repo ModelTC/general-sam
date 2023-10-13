9e148aded811c48bd23798167829f3966cb4c393
Chielo Newctle <ChieloNewctle@gmail.com>
2023-10-13 17:20:03 +0800

    add insert to trie

--- src/trie.rs
+++ src/trie.rs
@@ -5,6 +5,7 @@ use crate::trie_alike::TrieNodeAlike;
 #[derive(Debug, Clone)]
 pub struct Node<T: Ord + Clone> {
     trans: BTreeMap<T, usize>,
+    parent: usize,
     pub accept: bool,
 }
 
@@ -19,6 +20,27 @@ pub struct State<'s, T: Ord + Clone> {
     node_id: usize,
 }
 
+const TRIE_NIL_NODE_ID: usize = 0;
+const TRIE_ROOT_NODE_ID: usize = 1;
+
+impl<T: Ord + Clone> Node<T> {
+    fn new(parent: usize) -> Self {
+        Self {
+            trans: Default::default(),
+            parent,
+            accept: Default::default(),
+        }
+    }
+}
+
+impl<T: Ord + Clone> Default for Trie<T> {
+    fn default() -> Self {
+        Self {
+            node_pool: vec![Node::new(TRIE_NIL_NODE_ID), Node::new(TRIE_NIL_NODE_ID)],
+        }
+    }
+}
+
 impl<T: Ord + Clone> Trie<T> {
     pub fn get_state(&self, node_id: usize) -> State<T> {
         State {
@@ -26,11 +48,61 @@ impl<T: Ord + Clone> Trie<T> {
             node_id,
         }
     }
+
+    pub fn get_root(&self) -> State<T> {
+        self.get_state(TRIE_ROOT_NODE_ID)
+    }
+
+    fn alloc_node(&mut self, parent: usize) -> usize {
+        let node_id = self.node_pool.len();
+        self.node_pool.push(Node::new(parent));
+        node_id
+    }
+
+    pub fn insert_iter_ref<'s, Iter: Iterator<Item = &'s T>>(&'s mut self, iter: Iter) {
+        self.insert_iter(iter.cloned())
+    }
+
+    pub fn insert_iter<Iter: Iterator<Item = T>>(&mut self, iter: Iter) {
+        let mut current = TRIE_ROOT_NODE_ID;
+        iter.for_each(|t| {
+            current = match self.node_pool[current].trans.get(&t) {
+                Some(v) => *v,
+                None => {
+                    let new_node_id = self.alloc_node(current);
+                    self.node_pool[current].trans.insert(t, new_node_id);
+                    new_node_id
+                }
+            };
+        });
+        self.node_pool[current].accept = true;
+    }
 }
 
 impl<'s, T: Ord + Clone> State<'s, T> {
+    pub fn is_nil(&self) -> bool {
+        self.node_id == TRIE_NIL_NODE_ID
+    }
+
+    pub fn is_root(&self) -> bool {
+        self.node_id == TRIE_ROOT_NODE_ID
+    }
+
     pub fn get_node(&self) -> &'s Node<T> {
         &self.trie.node_pool[self.node_id]
+    }
+
+    pub fn goto_parent(&mut self) {
+        self.node_id = self.get_node().parent;
+    }
+
+    pub fn goto(&mut self, t: &T) {
+        self.node_id = self
+            .get_node()
+            .trans
+            .get(t)
+            .copied()
+            .unwrap_or(TRIE_NIL_NODE_ID)
     }
 }
 
