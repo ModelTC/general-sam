c84d3a93a4369fba394a1353edc2c75e62de9fee
Chielo Newctle <ChieloNewctle@gmail.com>
2023-10-13 17:20:18 +0800

    update tests and readme usage

--- pybind/README.md
+++ pybind/README.md
@@ -68,18 +68,23 @@ assert not state.is_accepting() and state.is_nil()
 ```
 
 ```python
-from general_sam import VocabPrefixAutomaton
+from general_sam import VocabPrefixAutomaton, CountInfo
 
 
 vocab = ['歌曲', '聆听歌曲', '播放歌曲', '歌词', '查看歌词']
 automaton = VocabPrefixAutomaton(vocab, bytes_or_chars='chars')
+
+# NOTE: CountInfo is related to the sorted vocab:
+_ = ['播放歌曲', '查看歌词', '歌曲', '歌词', '聆听歌曲']
 
 # 一起 | 聆 | 听 | 歌
 state = automaton.get_root_state()
 
 # feed 歌
 cnt_info = automaton.prepend_feed(state, '歌')
-assert cnt_info is not None
+assert cnt_info is not None and cnt_info == CountInfo(
+    str_cnt=2, tot_cnt_lower=2, tot_cnt_upper=4
+)
 
 selected_idx = automaton.get_order_slice(cnt_info)
 assert frozenset(selected_idx) == {0, 3}
@@ -93,7 +98,9 @@ assert not state.is_nil()
 
 # feed 聆
 cnt_info = automaton.prepend_feed(state, '聆')
-assert cnt_info is not None
+assert cnt_info is not None and cnt_info == CountInfo(
+    str_cnt=1, tot_cnt_lower=4, tot_cnt_upper=5
+)
 
 selected_idx = automaton.get_order_slice(cnt_info)
 assert frozenset(selected_idx) == {1}
@@ -110,7 +117,9 @@ state = automaton.get_root_state()
 
 # feed 歌词
 cnt_info = automaton.prepend_feed(state, '歌词')
-assert cnt_info is not None
+assert cnt_info is not None and cnt_info == CountInfo(
+    str_cnt=1, tot_cnt_lower=3, tot_cnt_upper=4
+)
 
 selected_idx = automaton.get_order_slice(cnt_info)
 assert frozenset(selected_idx) == {3}
@@ -119,7 +128,9 @@ assert frozenset(selected_vocab) == {'歌词'}
 
 # feed 查看
 cnt_info = automaton.prepend_feed(state, '查看')
-assert cnt_info is not None
+assert cnt_info is not None and cnt_info == CountInfo(
+    str_cnt=1, tot_cnt_lower=1, tot_cnt_upper=2
+)
 
 selected_idx = automaton.get_order_slice(cnt_info)
 assert frozenset(selected_idx) == {4}
--- pybind/tests/test_vocab_prefix.py
+++ pybind/tests/test_vocab_prefix.py
@@ -1,16 +1,21 @@
-from general_sam import VocabPrefixAutomaton
+from general_sam import VocabPrefixAutomaton, CountInfo
 
 
 def test_chinese_chars_vocab_prefix():
     vocab = ['歌曲', '聆听歌曲', '播放歌曲', '歌词', '查看歌词']
     automaton = VocabPrefixAutomaton(vocab, bytes_or_chars='chars')
 
+    # NOTE: CountInfo is related to the sorted vocab:
+    _ = ['播放歌曲', '查看歌词', '歌曲', '歌词', '聆听歌曲']
+
     # 一起 | 聆 | 听 | 歌
     state = automaton.get_root_state()
 
     # feed 歌
     cnt_info = automaton.prepend_feed(state, '歌')
-    assert cnt_info is not None
+    assert cnt_info is not None and cnt_info == CountInfo(
+        str_cnt=2, tot_cnt_lower=2, tot_cnt_upper=4
+    )
 
     selected_idx = automaton.get_order_slice(cnt_info)
     assert frozenset(selected_idx) == {0, 3}
@@ -24,7 +29,9 @@ def test_chinese_chars_vocab_prefix():
 
     # feed 聆
     cnt_info = automaton.prepend_feed(state, '聆')
-    assert cnt_info is not None
+    assert cnt_info is not None and cnt_info == CountInfo(
+        str_cnt=1, tot_cnt_lower=4, tot_cnt_upper=5
+    )
 
     selected_idx = automaton.get_order_slice(cnt_info)
     assert frozenset(selected_idx) == {1}
@@ -41,7 +48,9 @@ def test_chinese_chars_vocab_prefix():
 
     # feed 歌词
     cnt_info = automaton.prepend_feed(state, '歌词')
-    assert cnt_info is not None
+    assert cnt_info is not None and cnt_info == CountInfo(
+        str_cnt=1, tot_cnt_lower=3, tot_cnt_upper=4
+    )
 
     selected_idx = automaton.get_order_slice(cnt_info)
     assert frozenset(selected_idx) == {3}
@@ -50,7 +59,9 @@ def test_chinese_chars_vocab_prefix():
 
     # feed 查看
     cnt_info = automaton.prepend_feed(state, '查看')
-    assert cnt_info is not None
+    assert cnt_info is not None and cnt_info == CountInfo(
+        str_cnt=1, tot_cnt_lower=1, tot_cnt_upper=2
+    )
 
     selected_idx = automaton.get_order_slice(cnt_info)
     assert frozenset(selected_idx) == {4}
