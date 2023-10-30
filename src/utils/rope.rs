dac17ac85b7a0f807caa1e33e1aefd1f3e39c8fb
Chielo Newctle <ChieloNewctle@gmail.com>
2023-10-30 15:12:49 +0800

    feat: add generic persistent rope

---
+++ src/utils/rope.rs
@@ -0,0 +1,305 @@
+use std::ops::Deref;
+
+use super::treap::{NeedSwap, SplitTo, TreapNodeData, TreapTree};
+
+pub trait RopeData: Clone {
+    type TagType: Default;
+
+    fn get_tag(&self) -> Option<Self::TagType>;
+    fn reset_tag(&mut self);
+    fn add_tag(&mut self, tag: Self::TagType) -> NeedSwap;
+
+    fn update(&mut self, left: Option<&Self>, right: Option<&Self>);
+}
+
+#[derive(Clone)]
+pub struct RopeTreapData<Inner: RopeData> {
+    inner: Inner,
+    num: usize,
+    rev_tag: bool,
+}
+
+impl<Inner: RopeData> RopeTreapData<Inner> {
+    fn new(data: Inner) -> Self {
+        Self {
+            inner: data,
+            num: 1,
+            rev_tag: false,
+        }
+    }
+}
+impl<Inner: RopeData> Deref for RopeTreapData<Inner> {
+    type Target = Inner;
+
+    fn deref(&self) -> &Self::Target {
+        &self.inner
+    }
+}
+impl<Inner: RopeData> TreapNodeData for RopeTreapData<Inner> {
+    type TagType = (bool, Option<Inner::TagType>);
+    fn get_tag(&self) -> Option<Self::TagType> {
+        match (self.rev_tag, self.inner.get_tag()) {
+            (false, None) => None,
+            other => Some(other),
+        }
+    }
+    fn reset_tag(&mut self) {
+        self.rev_tag = false;
+        self.inner.reset_tag();
+    }
+    fn add_tag(&mut self, tag: Self::TagType) -> NeedSwap {
+        self.rev_tag ^= tag.0;
+        if let Some(inner_tag) = tag.1 {
+            self.inner.add_tag(inner_tag) ^ tag.0
+        } else {
+            tag.0
+        }
+    }
+    fn update(&mut self, left: Option<&Self>, right: Option<&Self>) {
+        self.inner
+            .update(left.map(|x| &x.inner), right.map(|x| &x.inner));
+        self.num = left.map_or(0, |x| x.num) + right.map_or(0, |x| x.num) + 1;
+    }
+}
+
+pub trait RopeBase: Sized + Clone {
+    type InnerRopeData: RopeData;
+
+    #[must_use]
+    fn new(data: Self::InnerRopeData) -> Self;
+    fn is_empty(&self) -> bool;
+    fn len(&self) -> usize;
+    fn for_each<F: FnMut(Self::InnerRopeData)>(&self, f: F);
+    #[must_use]
+    fn reverse(&self) -> Self;
+    #[must_use]
+    fn split(&self, num: usize) -> (Self, Self);
+    #[must_use]
+    fn merge(&self, other: &Self) -> Self;
+    fn root_data_ref(&self) -> Option<&Self::InnerRopeData>;
+    #[must_use]
+    fn add_tag(&self, tag: <Self::InnerRopeData as RopeData>::TagType) -> Self;
+
+    #[must_use]
+    fn insert(&self, pos: usize, data: Self::InnerRopeData) -> Self {
+        let (left, right) = self.split(pos);
+        left.merge(&Self::new(data)).merge(&right)
+    }
+
+    #[must_use]
+    fn remove(&self, pos: usize) -> (Self, Option<Self::InnerRopeData>) {
+        if pos >= self.len() {
+            return (self.clone(), None);
+        }
+        let (left, right) = self.split(pos);
+        let (middle, right) = right.split(1);
+        (left.merge(&right), middle.get(0))
+    }
+
+    #[must_use]
+    fn push_back(&self, data: Self::InnerRopeData) -> Self {
+        self.insert(self.len(), data)
+    }
+
+    #[must_use]
+    fn push_front(&self, data: Self::InnerRopeData) -> Self {
+        self.insert(0, data)
+    }
+
+    fn get(&self, pos: usize) -> Option<Self::InnerRopeData> {
+        self.split(pos).1.split(1).0.root_data_ref().cloned()
+    }
+}
+
+pub trait TreapBasedRopeBase: RopeBase {
+    fn new_from_rng<R: FnMut() -> usize>(data: Self::InnerRopeData, rng: R) -> Self;
+
+    fn insert_from_rng<R: FnMut() -> usize>(
+        &self,
+        pos: usize,
+        data: Self::InnerRopeData,
+        rng: R,
+    ) -> Self {
+        let (left, right) = self.split(pos);
+        left.merge(&Self::new_from_rng(data, rng)).merge(&right)
+    }
+
+    fn push_back_from_rng<R: FnMut() -> usize>(&self, data: Self::InnerRopeData, rng: R) -> Self {
+        self.insert_from_rng(self.len(), data, rng)
+    }
+
+    fn push_front_from_rng<R: FnMut() -> usize>(&self, data: Self::InnerRopeData, rng: R) -> Self {
+        self.insert_from_rng(0, data, rng)
+    }
+}
+
+impl<
+        InnerRopeData: RopeData,
+        TreapBasedRope: From<TreapTree<RopeTreapData<InnerRopeData>>>
+            + Deref<Target = TreapTree<RopeTreapData<InnerRopeData>>>
+            + Clone,
+    > TreapBasedRopeBase for TreapBasedRope
+{
+    fn new_from_rng<R: FnMut() -> usize>(data: Self::InnerRopeData, rng: R) -> Self {
+        TreapTree::new_from_rng(RopeTreapData::new(data), rng).into()
+    }
+}
+impl<
+        InnerRopeData: RopeData,
+        TreapBasedRope: From<TreapTree<RopeTreapData<InnerRopeData>>>
+            + Deref<Target = TreapTree<RopeTreapData<InnerRopeData>>>
+            + Clone,
+    > RopeBase for TreapBasedRope
+{
+    type InnerRopeData = InnerRopeData;
+
+    fn new(data: Self::InnerRopeData) -> Self {
+        TreapTree::new(RopeTreapData::new(data)).into()
+    }
+
+    fn is_empty(&self) -> bool {
+        self.is_some()
+    }
+
+    fn len(&self) -> usize {
+        self.deref().root_data_ref().map_or(0, |x| x.num)
+    }
+
+    fn for_each<F: FnMut(Self::InnerRopeData)>(&self, mut f: F) {
+        self.deref().for_each(&mut |x| f(x.inner))
+    }
+
+    fn reverse(&self) -> Self {
+        self.deref().add_tag((true, None)).into()
+    }
+
+    fn split(&self, mut num: usize) -> (Self, Self) {
+        let (u, v) = self.deref().split(|node| {
+            let to_left_num = node.get_left().deref().as_ref().map_or(0, |x| x.data.num) + 1;
+            if num >= to_left_num {
+                num -= to_left_num;
+                SplitTo::Left
+            } else {
+                SplitTo::Right
+            }
+        });
+        (u.into(), v.into())
+    }
+
+    fn merge(&self, other: &Self) -> Self {
+        self.deref().merge(other.deref()).into()
+    }
+
+    fn root_data_ref(&self) -> Option<&Self::InnerRopeData> {
+        self.deref().root_data_ref().map(|x| &x.inner)
+    }
+
+    fn add_tag(&self, tag: <Self::InnerRopeData as RopeData>::TagType) -> Self {
+        self.deref().add_tag((false, Some(tag))).into()
+    }
+}
+
+#[derive(Clone, Default)]
+pub struct Rope<Inner: RopeData>(TreapTree<RopeTreapData<Inner>>);
+
+impl<Inner: RopeData> From<TreapTree<RopeTreapData<Inner>>> for Rope<Inner> {
+    fn from(value: TreapTree<RopeTreapData<Inner>>) -> Self {
+        Self(value)
+    }
+}
+impl<Inner: RopeData> Deref for Rope<Inner> {
+    type Target = TreapTree<RopeTreapData<Inner>>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct RopeUntaggedInner<T: Clone>(T);
+
+impl<T: Clone> Deref for RopeUntaggedInner<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<T: Clone> From<T> for RopeUntaggedInner<T> {
+    fn from(value: T) -> Self {
+        Self(value)
+    }
+}
+
+impl<T: Clone> RopeData for RopeUntaggedInner<T> {
+    type TagType = ();
+    fn get_tag(&self) -> Option<Self::TagType> {
+        None
+    }
+    fn reset_tag(&mut self) {}
+    fn update(&mut self, _: Option<&Self>, _: Option<&Self>) {}
+    fn add_tag(&mut self, _: Self::TagType) -> NeedSwap {
+        false
+    }
+}
+
+pub type UntaggedRope<T> = Rope<RopeUntaggedInner<T>>;
+
+#[test]
+fn test_rope() {
+    let rope = UntaggedRope::<char>::default();
+    assert!(rope.get(0).is_none());
+
+    let rope = rope.push_front('a'.into());
+    assert!(rope.get(0).is_some_and(|x| *x == 'a'));
+
+    let rope = rope.push_front('b'.into());
+    let rope = rope.push_back('c'.into());
+    assert!(rope.get(0).is_some_and(|x| *x == 'b'));
+    assert!(rope.get(1).is_some_and(|x| *x == 'a'));
+    assert!(rope.get(2).is_some_and(|x| *x == 'c'));
+    assert!(rope.get(3).is_none());
+
+    let rope = rope.reverse();
+    assert!(rope.get(0).is_some_and(|x| *x == 'c'));
+    assert!(rope.get(1).is_some_and(|x| *x == 'a'));
+    assert!(rope.get(2).is_some_and(|x| *x == 'b'));
+    assert!(rope.get(3).is_none());
+
+    let (l, r) = rope.split(1);
+    assert!(rope.get(0).is_some_and(|x| *x == 'c'));
+    assert!(rope.get(1).is_some_and(|x| *x == 'a'));
+    assert!(rope.get(2).is_some_and(|x| *x == 'b'));
+    assert!(rope.get(3).is_none());
+    assert!(l.get(0).is_some_and(|x| *x == 'c'));
+    assert!(l.get(1).is_none());
+    assert!(r.get(0).is_some_and(|x| *x == 'a'));
+    assert!(r.get(1).is_some_and(|x| *x == 'b'));
+    assert!(r.get(2).is_none());
+
+    let to_vec = |p: UntaggedRope<char>| -> Vec<char> {
+        let mut res = Vec::<char>::new();
+        p.for_each(&mut |d: RopeUntaggedInner<char>| res.push(*d));
+        res
+    };
+
+    let reversed = rope.reverse();
+    let v = to_vec(reversed);
+    v.iter()
+        .zip(['b', 'a', 'c'])
+        .for_each(|(i, j)| assert_eq!(*i, j));
+
+    let v = to_vec(rope);
+    v.iter()
+        .zip(['c', 'a', 'b'])
+        .for_each(|(i, j)| assert_eq!(*i, j));
+
+    let v = to_vec(l);
+    v.iter().zip(['c']).for_each(|(i, j)| assert_eq!(*i, j));
+
+    let v = to_vec(r);
+    v.iter()
+        .zip(['a', 'b'])
+        .for_each(|(i, j)| assert_eq!(*i, j));
+}
---
+++ src/utils/treap.rs
@@ -0,0 +1,188 @@
+use std::{borrow::Cow, ops::Deref, sync::Arc};
+
+use rand::random;
+
+pub type NeedSwap = bool;
+
+pub enum SplitTo {
+    Left,
+    Right,
+}
+
+pub trait TreapNodeData: Clone {
+    type TagType: Default;
+
+    fn get_tag(&self) -> Option<Self::TagType>;
+    fn reset_tag(&mut self);
+    fn add_tag(&mut self, tag: Self::TagType) -> NeedSwap;
+
+    fn update(&mut self, left: Option<&Self>, right: Option<&Self>);
+}
+
+#[derive(Clone)]
+pub struct TreapTree<DataType: TreapNodeData>(Option<Arc<TreapNode<DataType>>>);
+
+#[derive(Clone)]
+pub struct TreapNode<DataType: TreapNodeData> {
+    pub data: DataType,
+    height: usize,
+    _left: TreapTree<DataType>,
+    _right: TreapTree<DataType>,
+}
+
+impl<DataType: TreapNodeData> TreapNode<DataType> {
+    fn new(data: DataType) -> Self {
+        Self {
+            data,
+            height: random(),
+            _left: Default::default(),
+            _right: Default::default(),
+        }
+    }
+
+    fn new_from_rng<R: FnMut() -> usize>(data: DataType, mut rng: R) -> Self {
+        Self {
+            data,
+            height: rng(),
+            _left: Default::default(),
+            _right: Default::default(),
+        }
+    }
+
+    fn update(&mut self) {
+        self.data.update(
+            self._left.as_ref().map(|x| &x.data),
+            self._right.as_ref().map(|x| &x.data),
+        )
+    }
+
+    fn add_tag(&mut self, tag: DataType::TagType) {
+        if self.data.add_tag(tag) {
+            std::mem::swap(&mut self._left, &mut self._right)
+        }
+    }
+
+    #[must_use]
+    pub fn get_left(&self) -> Cow<TreapTree<DataType>> {
+        match self.data.get_tag() {
+            Some(tag) => Cow::Owned(self._left.add_tag(tag)),
+            None => Cow::Borrowed(&self._left),
+        }
+    }
+
+    #[must_use]
+    pub fn get_right(&self) -> Cow<TreapTree<DataType>> {
+        match self.data.get_tag() {
+            Some(tag) => Cow::Owned(self._right.add_tag(tag)),
+            None => Cow::Borrowed(&self._right),
+        }
+    }
+
+    fn set_left(&mut self, left: TreapTree<DataType>) {
+        if let Some(tag) = self.data.get_tag() {
+            self._right = self._right.add_tag(tag);
+        }
+        self.data.reset_tag();
+        self._left = left;
+        self.update();
+    }
+
+    fn set_right(&mut self, right: TreapTree<DataType>) {
+        if let Some(tag) = self.data.get_tag() {
+            self._left = self._left.add_tag(tag);
+        }
+        self.data.reset_tag();
+        self._right = right;
+        self.update();
+    }
+}
+
+impl<DataType: TreapNodeData> Default for TreapTree<DataType> {
+    fn default() -> Self {
+        Self(None)
+    }
+}
+impl<DataType: TreapNodeData> Deref for TreapTree<DataType> {
+    type Target = Option<Arc<TreapNode<DataType>>>;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl<DataType: TreapNodeData> TreapTree<DataType> {
+    pub fn new(data: DataType) -> Self {
+        Self(Some(Arc::new(TreapNode::new(data))))
+    }
+
+    pub fn new_from_rng<R: FnMut() -> usize>(data: DataType, rng: R) -> Self {
+        Self(Some(Arc::new(TreapNode::new_from_rng(data, rng))))
+    }
+
+    pub fn root_data_ref(&self) -> Option<&DataType> {
+        self.as_ref().map(|x| &x.data)
+    }
+
+    #[must_use]
+    pub fn map<F: FnOnce(&mut TreapNode<DataType>)>(&self, f: F) -> Self {
+        if let Some(node_ref) = self.deref() {
+            let mut node = node_ref.deref().clone();
+            f(&mut node);
+            Self(Some(Arc::new(node)))
+        } else {
+            Self::default()
+        }
+    }
+
+    #[must_use]
+    pub fn add_tag(&self, tag: DataType::TagType) -> Self {
+        self.map(|node| node.add_tag(tag))
+    }
+
+    #[must_use]
+    pub fn merge(&self, other: &Self) -> Self {
+        match (self.deref(), other.deref()) {
+            (None, None) => Self::default(),
+            (None, Some(_)) => other.clone(),
+            (Some(_), None) => self.clone(),
+            (Some(left), Some(right)) => {
+                if left.height > right.height {
+                    let mut u = left.deref().to_owned();
+                    u.set_right(u.get_right().merge(other));
+                    Self(Some(Arc::new(u)))
+                } else {
+                    let mut v = right.deref().to_owned();
+                    v.set_left(self.merge(&v.get_left()));
+                    Self(Some(Arc::new(v)))
+                }
+            }
+        }
+    }
+
+    #[must_use]
+    pub fn split<F: FnMut(&mut TreapNode<DataType>) -> SplitTo>(&self, mut f: F) -> (Self, Self) {
+        if let Some(ref node_ref) = self.deref() {
+            let mut node = node_ref.deref().clone();
+            match f(&mut node) {
+                SplitTo::Left => {
+                    let (left, right) = node.get_right().split(f);
+                    node.set_right(left);
+                    (Self(Some(Arc::new(node))), right)
+                }
+                SplitTo::Right => {
+                    let (left, right) = node.get_left().split(f);
+                    node.set_left(right);
+                    (left, Self(Some(Arc::new(node))))
+                }
+            }
+        } else {
+            (Self::default(), Self::default())
+        }
+    }
+
+    pub fn for_each<F: FnMut(DataType)>(&self, f: &mut F) {
+        if let Some(node_ref) = self.deref() {
+            node_ref.get_left().for_each(f);
+            f(node_ref.data.clone());
+            node_ref.get_right().for_each(f);
+        }
+    }
+}
